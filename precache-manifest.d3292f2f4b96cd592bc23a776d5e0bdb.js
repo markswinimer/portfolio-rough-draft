850165150b7ca91d1c623834d451443bc5f9cdd7
markswinimer <markswinimer@gmail.com>
2019-10-14 12:18:16 -0400

    Updates

---
+++ precache-manifest.d3292f2f4b96cd592bc23a776d5e0bdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e83aa5dd2ac1d5941df92fc430d0b0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5c76f4e9ac78170af610",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1f755a97e117436dae96",
+    "url": "/portfolio/static/js/2.99c828d0.chunk.js"
+  },
+  {
+    "revision": "5c76f4e9ac78170af610",
+    "url": "/portfolio/static/js/main.8dd43726.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  }
+]);
