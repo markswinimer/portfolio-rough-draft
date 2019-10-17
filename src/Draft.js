da29c55fdaac3925cb13691ce446dbaa6932467e
markswinimer <markswinimer@gmail.com>
2019-10-16 21:56:04 -0400

    quick mock up

--- src/Draft.js
+++ src/Draft.js
@@ -9,8 +9,33 @@ import "./Draft.css";
    render() {
      return(
        //Initial drafting phase for html/css skeleton
-       <div className="app">
-
+       <div className="Home-container">
+         <div className="Home">
+           <div className="Home-name">
+             <h1>Mark Swinimer</h1>
+             <h2>Web Developer</h2>
+           </div>
+           <div className="Home-intro">
+             <p>
+               Hello my name is Mark Swinimer. I'm a web developer working in the Greater Boston area. My passion is writing creating clean, creative, and functional solutions.
+             </p>
+           </div>
+           <div className="Home-content">
+             <div className="Home-project">
+               <div className="tag">
+                 TAG
+               </div>
+               <div className="box">
+                 <img src="https://assets.pcmag.com/media/images/521567-how-to-build-a-website.jpg?thumb=y&width=810&height=456"/>
+               </div>
+             </div>
+             <div className="Home-project-info">
+               <h2>Project</h2>
+               <p>information about mark</p>
+               <p>stats on mark</p>
+             </div>
+           </div>
+         </div>
        </div>
      )
    }
